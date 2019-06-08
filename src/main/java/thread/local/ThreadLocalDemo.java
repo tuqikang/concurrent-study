cd9e573e82ad840f6cfcbcca936cb016f2d1f219
tqk <1282995880@qq.com>
2019-06-08 13:56:44 +0800

    ThreadLocalDemo

---
+++ src/main/java/thread/local/ThreadLocalDemo.java
@@ -0,0 +1,28 @@
+package thread.local;
+
+/**
+ * █████▒█      ██  ▄████▄   ██ ▄█▀       ██████╗ ██╗   ██╗ ██████╗
+ * ▓██   ▒ ██  ▓██▒▒██▀ ▀█   ██▄█▒        ██╔══██╗██║   ██║██╔════╝
+ * ▒████ ░▓██  ▒██░▒▓█    ▄ ▓███▄░        ██████╔╝██║   ██║██║  ███╗
+ * ░▓█▒  ░▓▓█  ░██░▒▓▓▄ ▄██▒▓██ █▄        ██╔══██╗██║   ██║██║   ██║
+ * ░▒█░   ▒▒█████▓ ▒ ▓███▀ ░▒██▒ █▄       ██████╔╝╚██████╔╝╚██████╔╝
+ * ▒ ░   ░▒▓▒ ▒ ▒ ░ ░▒ ▒  ░▒ ▒▒ ▓▒       ╚═════╝  ╚═════╝  ╚═════╝
+ * ░     ░░▒░ ░ ░   ░  ▒   ░ ░▒ ▒░
+ * ░ ░    ░░░ ░ ░ ░        ░ ░░ ░
+ * ░     ░ ░      ░  ░
+ *
+ * @author ：涂齐康
+ * @date ：Created in 2019-06-08 13:52
+ * @description：
+ * @modified By：
+ * @version:
+ */
+public class ThreadLocalDemo {
+    public static void main(String[] args) {
+        ThreadLocal<String> local = new ThreadLocal<>();
+        local.set("hello world");
+        System.out.println("当前线程"+Thread.currentThread().getName());
+        System.out.println("从当前线程获得local存储的变量"+local.get());
+        new Thread(()-> System.out.println("我是"+Thread.currentThread().getName()+",我要从local中获得它存储的对象:"+local.get()),"新线程").start();
+    }
+}
