3eef7f0c09a5f14fd9d25521beba264a688453c5
tqk <1282995880@qq.com>
2019-06-10 10:48:46 +0800

    threadlocal

--- src/main/java/thread/local/CodeBearThreadLocal.java
+++ src/main/java/thread/local/CodeBearThreadLocal.java
@@ -1,6 +1,6 @@
 package thread.local;
 
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * █████▒█      ██  ▄████▄   ██ ▄█▀       ██████╗ ██╗   ██╗ ██████╗
@@ -14,26 +14,27 @@ import java.util.concurrent.TimeUnit;
  * ░     ░ ░      ░  ░
  *
  * @author ：涂齐康
- * @date ：Created in 2019-06-08 13:52
- * @description：
+ * @date ：Created in 2019-06-08 16:01
+ * @description 自己写个ThreadLocal
  * @modified By：
  * @version:
  */
-public class ThreadLocalDemo {
-    public static void main(String[] args) throws InterruptedException {
-        Thread t= new Thread();
-        ThreadLocal<String> local1 = new ThreadLocal<>();
-        local1.set("hello world");
-//        local1.set("tuqikang");
-        ThreadLocal<String> local2 = new ThreadLocal<>();
-        local2.set("hello world 2");
-        System.out.println("当前线程"+Thread.currentThread().getName());
-        TimeUnit.SECONDS.sleep(10);
-        System.out.println("从当前线程获得local存储的变量"+local1.get());
-        System.out.println(local2.get());
-        new Thread(()-> {
-            local1.set("new  hello world");
-            System.out.println("我是"+Thread.currentThread().getName()+",我要从local中获得它存储的对象:"+local1.get());
-        },"新线程").start();
+public class CodeBearThreadLocal<T> {
+
+    private ConcurrentHashMap<Long,T> hashMap = new ConcurrentHashMap<>(16);
+
+    public void set(T t){
+        hashMap.put(Thread.currentThread().getId(),t);
+    }
+
+    public T get(){
+        return hashMap.get(Thread.currentThread().getId());
+    }
+
+    public static void main(String[] args) {
+        CodeBearThreadLocal<String> codeBearThreadLocal = new CodeBearThreadLocal<>();
+        codeBearThreadLocal.set("hello world");
+        System.out.println(codeBearThreadLocal.get());
+        new Thread(()-> System.out.println(codeBearThreadLocal.get())).start();
     }
 }
