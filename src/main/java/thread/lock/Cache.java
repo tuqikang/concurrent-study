f744c67832d9d86c5173b01628dcff9d19905424
tqk <1282995880@qq.com>
2019-06-17 20:36:37 +0800

    通过读写锁完成一个cache

---
+++ src/main/java/thread/lock/Cache.java
@@ -0,0 +1,74 @@
+package thread.lock;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+/**
+ * █████▒█      ██  ▄████▄   ██ ▄█▀       ██████╗ ██╗   ██╗ ██████╗
+ * ▓██   ▒ ██  ▓██▒▒██▀ ▀█   ██▄█▒        ██╔══██╗██║   ██║██╔════╝
+ * ▒████ ░▓██  ▒██░▒▓█    ▄ ▓███▄░        ██████╔╝██║   ██║██║  ███╗
+ * ░▓█▒  ░▓▓█  ░██░▒▓▓▄ ▄██▒▓██ █▄        ██╔══██╗██║   ██║██║   ██║
+ * ░▒█░   ▒▒█████▓ ▒ ▓███▀ ░▒██▒ █▄       ██████╔╝╚██████╔╝╚██████╔╝
+ * ▒ ░   ░▒▓▒ ▒ ▒ ░ ░▒ ▒  ░▒ ▒▒ ▓▒       ╚═════╝  ╚═════╝  ╚═════╝
+ * ░     ░░▒░ ░ ░   ░  ▒   ░ ░▒ ▒░
+ * ░ ░    ░░░ ░ ░ ░        ░ ░░ ░
+ * ░     ░ ░      ░  ░
+ *
+ * @author ：涂齐康
+ * @date ：Created in 2019-06-17 20:30
+ * @description：
+ * @modified By：
+ * @version:
+ */
+public class Cache {
+
+    private static Map<String, Object> map = new HashMap<>(16);
+    private static ReentrantReadWriteLock rwl = new ReentrantReadWriteLock();
+    private static Lock r = rwl.readLock();
+    private static Lock w = rwl.writeLock();
+
+    /**
+     * 获取key对应的value
+     *
+     * @param key
+     * @return
+     */
+    public static final Object get(String key) {
+        r.lock();
+        try {
+            return map.get(key);
+        } finally {
+            r.unlock();
+        }
+    }
+
+    /**
+     * 设置key对应的value，并返回旧的value
+     *
+     * @param key
+     * @param value
+     * @return
+     */
+    public static final Object put(String key, Object value) {
+        w.lock();
+        try {
+            return map.put(key, value);
+        } finally {
+            w.unlock();
+        }
+    }
+
+    /**
+     * 清空所有缓存
+     */
+    public static final void clear() {
+        w.lock();
+        try {
+            map.clear();
+        } finally {
+            w.unlock();
+        }
+    }
+}
